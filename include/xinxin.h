c4f1f28fbba7209f9f254b8c705825b2fa8a8f0f
HeapPie <absolutehollow0@gmail.com>
2022-11-11 10:15:13 +0800

    新文件：   Makefile 	新文件：   include/xinxin.h 	新文件：   src/hello_git.c

---
+++ include/xinxin.h
@@ -0,0 +1,30 @@
+/*
+ *  xinxin
+ *  A practice of data structure.
+ *  HeapPie
+ *  Licensed under MIT
+ *  https://github.com/HeapPie/xinxin
+ *  ----------------------------------------------------
+ *  Thanks to Daniel Heolden for his libCello.
+ *  libCello is the frist program that I read in github.
+ *  I am a beginner in C who learns so much from this.
+ *  https://github.com/orangeduck/libCello
+ * */
+
+/* Settings */
+
+/* Includes */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Syntax */
+
+/* Declaration */
+
+/* Types */
+
+/* classes */
+
+/* Signatures */
+
+/* Functions */
